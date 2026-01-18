b6d723fc2e999291aaa7e022f61091fd74fddb7c
71104 <a71104@gmail.com>
2026-01-18 21:01:50 +0100

    Fixes.

--- src/renderer/System.tsx
+++ src/renderer/System.tsx
@@ -4,12 +4,19 @@ import { ControlBar } from "./ControlBar";
 import { Page as SettingsPage } from "./Settings";
 import { Page as WalletPage } from "./Wallet";
 
-function getRoute(): string {
-  return new URLSearchParams(window.location.search).get("route");
+function getRoute(): string[] {
+  return new URLSearchParams(window.location.search).get("route").split("/");
 }
 
 export const System = () => {
-  const view = useMemo<string | null>(() => getRoute(), []);
+  const view = useMemo<string | null>(() => {
+    const route = getRoute();
+    if (route.length > 0) {
+      return route[0];
+    } else {
+      return null;
+    }
+  }, []);
   switch (view) {
     case "control":
       return <ControlBar />;
