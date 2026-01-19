9a608417e1bbc31299ed96368cc9769e513a2272
71104 <a71104@gmail.com>
2026-01-19 03:15:39 +0100

    Minor tweaks.

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,9 @@ if (require("electron-squirrel-startup")) {
 
 // This method will be called when Electron has finished initialization and is ready to create
 // browser windows. Some APIs can only be used after this event occurs.
-app.on("ready", () => BrowserWindow.create());
+app.on("ready", async () => {
+  await BrowserWindow.create();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common for applications and their
 // menu bar to stay active until the user quits explicitly with Cmd + Q.
@@ -37,11 +39,11 @@ app.on("window-all-closed", () => {
   }
 });
 
-app.on("activate", () => {
+app.on("activate", async () => {
   // On OS X it's common to re-create a window in the app when the dock icon is clicked and there
   // are no other windows open.
   if (ElectronBrowserWindow.getAllWindows().length === 0) {
-    BrowserWindow.create();
+    await BrowserWindow.create();
   }
 });
 
